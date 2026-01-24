f564218f360756847e72fb0c2fe3d79cf21b26c0
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2026-01-24 12:23:36 -0800

    change looks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,7 +13,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Sequel Sans', 'system-ui', 'sans-serif'],
       },
       colors: {
         border: "hsl(var(--border))",
